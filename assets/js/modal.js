b428e00b1b2fe10599ffe1a9a38d654cdcd5b158
Sandra Krle <sandra@edqu.se>
2016-11-19 19:26:05 +0100

    Initializing

---
+++ assets/js/modal.js
@@ -0,0 +1,28 @@
+// Get the modal
+var modal1 = document.getElementById('myModal-1');
+var modal2 = document.getElementById('myModal-2');
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("btn-modal-1");
+var btn2 = document.getElementById("btn-modal-2");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
---
+++ assets/js/typed.js
@@ -0,0 +1,10 @@
+$(function(){
+    $("#typed").typed({
+        stringsElement: $('#typed-strings'),
+        loop: true,
+        loopCount: false,
+        typeSpeed: 300,
+    	starDelay: 200,
+    	backDelay: 600
+    });
+});
