a0b53bdc6ad6eb79a0a8c40694ca0216595f6473
Sandra Krle <sandra@edqu.se>
2016-11-23 21:49:23 +0100

    Fix footer

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,25 +5,27 @@ $(function() {
 	clientStuff();
 
 	$("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
-	$(".biglink").fitText(1.5);
 
 	$('textarea').autosize();
 });
 
 // smoothScroll function is applied from the document ready function
 function smoothScroll (duration) {
-	$('a[href^="#"]').on('click', function(event) {
-
-	    var target = $( $(this).attr('href') );
-
-	    if( target.length ) {
-	        event.preventDefault();
-	        $('html, body').animate({
-	            scrollTop: target.offset().top
-	        }, duration);
-	    }
-	});
+	$('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 }
+
+
 
 function workBelt() {
 
