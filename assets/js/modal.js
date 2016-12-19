202ab277175bc6010ed40e6c8439a40e435bcbf4
SandraKrle <sandra.krlle@gmail.com>
2016-12-19 13:45:38 +0100

    Add close button

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -9,6 +9,8 @@ var btn2 = document.getElementById("btn-modal-2");
 // Get the <span> element that closes the modal
 var span1 = document.getElementsByClassName("close1")[0];
 var span2 = document.getElementsByClassName("close2")[0];
+var closeMe1 = document.getElementsByClassName("btn-close-modal-1")[0];
+var closeMe2 = document.getElementsByClassName("btn-close-modal-2")[0];
 
 // When the user clicks the button, open the modal 
 btn1.onclick = function() {
@@ -24,5 +26,13 @@ span1.onclick = function() {
     modal1.style.display = "none";
 }
 span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeMe1.onclick = function() {
+    modal1.style.display = "none";
+}
+closeMe2.onclick = function() {
     modal2.style.display = "none";
 }
